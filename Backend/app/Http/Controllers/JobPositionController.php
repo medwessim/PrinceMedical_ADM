11a53d5d696baeffc186fbd4921e980413d20041
wessim <wessimsaidani56@gmail.com>
2024-01-18 15:09:58 +0000

    database file

---
+++ Backend/app/Http/Controllers/JobPositionController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\JobPosition;
+use App\Http\Requests\StoreJobPositionRequest;
+use App\Http\Requests\UpdateJobPositionRequest;
+
+class JobPositionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreJobPositionRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(JobPosition $jobPosition)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(JobPosition $jobPosition)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateJobPositionRequest $request, JobPosition $jobPosition)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(JobPosition $jobPosition)
+    {
+        //
+    }
+}
--- Backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ Backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,13 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('lastName');
+            $table->string('userName')->unique();
             $table->string('password');
+            $table->string('photo');
+            $table->tinyInteger('isAdmin');
+            $table->foreign('group_id')->references('id')->on('Groups');
+            $table->foreign('position_id')->references('id')->on('JobPosition');
             $table->rememberToken();
             $table->timestamps();
         });

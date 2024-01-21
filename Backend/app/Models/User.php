58b71c21816d860169df79fa9e963dd117c222d7
wessim <wessimsaidani56@gmail.com>
2024-01-21 12:37:06 +0000

    models relation

--- Backend/app/Models/User.php
+++ Backend/app/Models/User.php
@@ -42,4 +42,14 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function jobPosition(){
+        $this->belongsTo(JobPosition::class);
+    }
+
+    public function group(){
+        $this->belongsTo(Group::class);
+    }
+
+
 }

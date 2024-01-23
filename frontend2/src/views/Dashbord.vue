dbc10cbc38857eb97c4840eeee46b006e3f127c8
wessim <wessimsaidani56@gmail.com>
2024-01-23 11:58:45 +0000

    dashbord page

---
+++ frontend2/src/views/Dashbord.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="home">
+      <navbar msg="Dashboard"></navbar>
+    </div>
+  </template>
+  
+  <script>
+  // @ is an alias to /src
+  
+  import navbar from '@/components/layout/navbar.vue'
+  
+  
+  export default {
+    name: 'Dashboard',
+    components: {
+      navbar,
+      
+      
+    }
+  }
+  </script>

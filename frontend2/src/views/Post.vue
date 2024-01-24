555bcae1d8f895febb07815873662c880dd5c43e
wessim <wessimsaidani56@gmail.com>
2024-01-24 13:54:58 +0000

    post page

---
+++ frontend2/src/views/Post.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="home">
+      <navbar msg="Post" ></navbar>
+      <PostTable></PostTable>
+    </div>
+  </template>
+  
+  <script>
+  // @ is an alias to /src
+  
+  import navbar from '@/components/layout/navbar.vue'
+  import PostTable from '@/components/layout/PostTable.vue'
+  
+  
+  export default {
+    name: 'Post',
+    components: {
+      navbar,
+      PostTable
+      
+    }
+  }
+  </script>
